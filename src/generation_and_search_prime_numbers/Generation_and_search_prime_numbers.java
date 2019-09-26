54f1afb01295a1ea2c164200d13a212a3b8e4e69
pupysheva <anime-manga@bk.ru>
2019-09-26 21:02:03 +0300

    Добавлен 2 пункт теста Рабина-Миллера

--- src/generation_and_search_prime_numbers/Generation_and_search_prime_numbers.java
+++ src/generation_and_search_prime_numbers/Generation_and_search_prime_numbers.java
@@ -9,6 +9,7 @@ import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 /**
  *
@@ -16,7 +17,7 @@ import java.util.List;
  */
 public class Generation_and_search_prime_numbers {
     //Число разрядов числа
-    static int n = 50; n++;
+    static int n = 10;
     static int N = 2000;
     static BigInteger p = BigInteger.ZERO;
     static List<BigInteger> list_prime_num = new ArrayList<>();;
@@ -27,10 +28,10 @@ public class Generation_and_search_prime_numbers {
             sieve_of_eratosthenes();
         }while(isDivided_into_simple());
         
-        int b  = 0;
-        for(int i=0;i<5;i++){
-            b = robin_miller_test();
-            System.out.println(b);
+        //int b  = 0;
+        for(int i=0;i<1;i++){
+            robin_miller_test();
+            //System.out.println(b);
         }
         
     }
@@ -77,31 +78,48 @@ public class Generation_and_search_prime_numbers {
         return flag_return;
     }
 
-    private static int robin_miller_test() {
+    private static void robin_miller_test() {
         
         //остановилася тута
         //сколько раз число делится на 2
-        int k = 0;
+        int b = 0;
         BigInteger TWO = BigInteger.valueOf(2);
         BigInteger temp_num = p.subtract(BigInteger.ONE);
         while(temp_num.compareTo(BigInteger.ZERO) == 1) { //если temp_num>0
             if(temp_num.mod(TWO).compareTo(BigInteger.ZERO)==0){
-                k++;
+                b++;
                 temp_num = temp_num.divide(TWO);
             }
             else
             {
-                return k;
+                break;
+                //return b;
             }
         }
-        return k;
+        //return b;
         
-        //Вычислить m
         
+        
+        System.out.println("b = "+b);
+        //Вычислить m = (p-1)/(2^b) (разделится без остатка)
+        BigInteger m = (    p.subtract(BigInteger.ONE)    ).divide(    BigInteger.valueOf((int)(Math.pow(2,b)))    );
+        System.out.println("m = "+m);
         //Выбрать случайное а
-        
+        BigInteger a = nextRandomBigInteger();
+        System.out.println("a = "+a);
         //Установить j=0 и
         
         
+    }
+    public static BigInteger nextRandomBigInteger() {
+        BigInteger temp_n = BigInteger.valueOf(n);
+        Random rand = new Random();
+        BigInteger result = new BigInteger(temp_n.bitLength(), rand);
+        while( result.compareTo(temp_n) >= 0 ) {//пока result>n
+            System.out.println("result = "+result);
+            result = new BigInteger(temp_n.bitLength(), rand);
+        }
+        
+        return result;
     }
 }
