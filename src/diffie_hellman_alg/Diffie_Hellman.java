84496bae17d0692c0f908ac309e255e3fdccc5d1
pupysheva <anime-manga@bk.ru>
2019-10-05 09:58:30 +0300

    Перевела в BigInteger.

---
+++ src/diffie_hellman_alg/Diffie_Hellman.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package diffie_hellman_alg;
+import static testing_bits_of_number.Testing_bits_of_number.generation_prime_num;//Имя пакет, класс, имя метода
+
+/**
+ *
+ * @author pupys
+ */
+public class Diffie_Hellman {
+    /*Протокол Ди́ффи — Хе́ллмана (англ. Diffie–Hellman, DH) — криптографический протокол, позволяющий двум и более сторонам получить общий секретный ключ, 
+    используя незащищенный от прослушивания канал связи. Полученный ключ используется для шифрования дальнейшего обмена с помощью алгоритмов симметричного шифрования.*/
+    
+    /*Симметри́чные криптосисте́мы (также симметричное шифрование, симметричные шифры) (англ. symmetric-key algorithm) — способ шифрования, в котором для шифрования и расшифровывания применяется один и тот же криптографический ключ.*/
+    public static void main(String[] args) {
+        p(20);
+    }
+    private static long p(int bits){
+        long p = generation_prime_num(bits);
+        System.out.println(p);
+        return p;
+    }
+}
--- src/generation_and_search_prime_numbers/Generation_and_search_prime_numbers.java
+++ src/generation_and_search_prime_numbers/Generation_and_search_prime_numbers.java
@@ -5,7 +5,7 @@
  */
 package generation_and_search_prime_numbers;
 
-import static testing_bits_of_number.Testing_bits_of_number.generation_prime_num;
+import static testing_bits_of_number.Testing_bits_of_number.generation_prime_num;//Имя пакет, класс, имя метода
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -18,35 +18,51 @@ import java.util.Random;
  */
 public class Generation_and_search_prime_numbers {
     //Число разрядов числа
-    static int n = 10;
+    static BigInteger num_p = BigInteger.ZERO;
+    static int n = 50;
     static int N = 2000;
-    static BigInteger p = BigInteger.ZERO;
     static List<BigInteger> list_prime_num = new ArrayList<>();
+    static int K = 5;
     
     public static void main(String[] args) {
-        /*do{
-            generation_n_bit_random_number();
-            System.out.println("\n"+p);
-            sieve_of_eratosthenes();
-        }while(isDivided_into_simple());
-        
-        //int b  = 0;
-        for(int i=0;i<1;i++){
-            robin_miller_test();
-            //System.out.println(b);
-        }*/
-        long a = generation_prime_num();
-        System.out.println(a);
+        /*long a = generation_prime_num(50);
+        System.out.println(a);*/
+        generation_Big_prime_num();
         
     }
+    public static void generation_Big_prime_num() {
+        boolean c = false;
+        while(!c){
+            do{
+                generation_n_bit_random_number();
+                sieve_of_eratosthenes();
+            }while(isDivided_into_simple());
+
+            //тест Робина-Миллера
+            c = rabin_miller_test();
+        }
+        System.out.println(num_p);
+    }
     static public void generation_n_bit_random_number(){
-        String s = "1";
+        /*String s = "1";
         for(int i=1;i<n-1;i++){
             int temp_didit = (int)(Math.random()*10);
             s=s+temp_didit;
         }
         s+=1;
-        p = new BigInteger(s);
+        num_p = new BigInteger(s);
+        System.out.println("p = "+ num_p);*/
+        num_p = BigInteger.ZERO;
+        for(int j = 0; j < n; j++)
+            num_p = (num_p.multiply(BigInteger.valueOf(2))).add(BigInteger.valueOf((int) (Math.random()*2)));
+        System.out.println("---------------------------------------------------");
+        System.out.println(num_p);
+        //num_p |=(1L<<(n-1)) +1L;
+        //System.out.println(num_p);
+       //System.out.println(Long.toBinaryString(num_p));
+        //System.out.println(Math.log(num_p)/Math.log(2));
+        //System.out.println("---------------------------------------------------");
+       
     }
     
     static public void sieve_of_eratosthenes(){
@@ -73,8 +89,8 @@ public class Generation_and_search_prime_numbers {
         boolean flag_return = false;
         for(BigInteger j : list_prime_num){
             //если p делится на число (если остаток ==0)
-           if(p.mod(j).compareTo(BigInteger.ZERO) == 0) {
-               System.out.println(p+"  "+j+" "+flag_return);
+           if(num_p.mod(j).compareTo(BigInteger.ZERO) == 0) {
+               System.out.println(num_p+"  "+j+" "+flag_return);
                return true;
            }
         }
@@ -82,48 +98,67 @@ public class Generation_and_search_prime_numbers {
         return flag_return;
     }
 
-    private static void robin_miller_test() {
-        
-        //остановилася тута
+    private static boolean rabin_miller_test() {
         //сколько раз число делится на 2
-        int b = 0;
-        BigInteger TWO = BigInteger.valueOf(2);
-        BigInteger temp_num = p.subtract(BigInteger.ONE);
+        BigInteger b = BigInteger.ZERO;
+        BigInteger BIGINTEGER_TWO = BigInteger.valueOf(2);
+        BigInteger temp_num = num_p.subtract(BigInteger.ONE);
         while(temp_num.compareTo(BigInteger.ZERO) == 1) { //если temp_num>0
-            if(temp_num.mod(TWO).compareTo(BigInteger.ZERO)==0){
-                b++;
-                temp_num = temp_num.divide(TWO);
+            if(temp_num.mod(BIGINTEGER_TWO).compareTo(BigInteger.ZERO)==0){
+                b.add(BigInteger.ONE);
+                temp_num = temp_num.divide(BIGINTEGER_TWO);
             }
-            else
-            {
+            else{
                 break;
-                //return b;
             }
         }
-        //return b;
-        
-        
         
         System.out.println("b = "+b);
         //Вычислить m = (p-1)/(2^b) (разделится без остатка)
-        BigInteger m = (    p.subtract(BigInteger.ONE)    ).divide(    BigInteger.valueOf((int)(Math.pow(2,b)))    );
+        BigInteger m = (num_p.subtract(BigInteger.ONE)).divide(BIGINTEGER_TWO.pow(b.intValue()));
         System.out.println("m = "+m);
-        //Выбрать случайное а
-        BigInteger a = nextRandomBigInteger();
-        System.out.println("a = "+a);
-        //Установить j=0 и
+        
+        for(int i = 0;i<K;i++){
+            //Выбрать случайное а<num_p. а должно быть: 1 < a < n
+            BigInteger a = nextRandomBigInteger();;
+            System.out.println("a = "+a);
+            
+            
+            //Установить j=0
+            int j =0;
+            BigInteger z = a.modPow(m, num_p);
+            System.out.println("z = a ** m % num_p = " + a +" ^ " + m + " mod " + num_p + " = " + z);
+            
+            if (z.compareTo(BigInteger.ONE) == 0 || z.compareTo(num_p.subtract(BigInteger.ONE) ) == 0){continue;}//то мб и простым
+            
+            boolean flag_while = true;
+            while(flag_while){
+                flag_while = false;
+                if(j>0 && z.compareTo(BigInteger.ONE) == 0) {return false;}// num_p  не простое, точка выхода
+                j+=1;
+                if(BigInteger.valueOf(j).compareTo(b) ==-1 && z.compareTo(num_p.subtract(BigInteger.ONE)) == -1){
+                    z = z.modPow(BIGINTEGER_TWO, num_p);
+                    flag_while = true;
+                    continue;
+                }
+                if(z.compareTo(num_p.subtract(BigInteger.ONE) ) == 0){flag_while = false; break;}//то мб и простым
+                if(b.compareTo(BigInteger.valueOf(j))==0 && z.compareTo(num_p.subtract(BigInteger.ONE)) !=0) return false;// num_p  не простое, точка выхода
+            }
+        }
+        return true;
         
         
     }
     public static BigInteger nextRandomBigInteger() {
-        BigInteger temp_n = BigInteger.valueOf(n);
+        /*Следующий метод использует конструктор BigInteger(int numBits, Random rnd) и отклоняет результат, если он больше указанного n.*/
+        BigInteger temp_n = new BigInteger(num_p+"");
         Random rand = new Random();
         BigInteger result = new BigInteger(temp_n.bitLength(), rand);
         while( result.compareTo(temp_n) >= 0 ) {//пока result>n
             System.out.println("result = "+result);
             result = new BigInteger(temp_n.bitLength(), rand);
         }
-        
+        System.out.println("result = "+result);
         return result;
     }
 }
