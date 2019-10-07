265ea7666d61de04c009a6e1bf6d69d99e60e026
pupysheva <anime-manga@bk.ru>
2019-10-07 12:28:39 +0300

    Update DiffieHellman.java

--- src/diffie_hellman_alg/DiffieHellman.java
+++ src/diffie_hellman_alg/DiffieHellman.java
@@ -4,7 +4,9 @@
  * and open the template in the editor.
  */
 package diffie_hellman_alg;
-import static testing_bits_of_number.Testing_bits_of_number.generation_prime_num;//Имя пакет, класс, имя метода
+import java.math.BigInteger;
+import java.util.*;
+import testing_bits_of_number.BigIntPrimeNumber;//.generationBigPrimeNum;//Имя пакет, класс, имя метода
 
 /**
  *
@@ -15,21 +17,92 @@ public class DiffieHellman {
     используя незащищенный от прослушивания канал связи. Полученный ключ используется для шифрования дальнейшего обмена с помощью алгоритмов симметричного шифрования.*/
     
     /*Симметри́чные криптосисте́мы (также симметричное шифрование, симметричные шифры) (англ. symmetric-key algorithm) — способ шифрования, в котором для шифрования и расшифровывания применяется один и тот же криптографический ключ.*/
-    public static long aAlice;
-    public static long gAlice;
-    public static long pAlice;
-    public static long A_Alice;
-    public static long kAlice;
+    public static BigInteger aAlice;
+    public static BigInteger gAlice;
+    public static BigInteger pAlice;
+    public static BigInteger A_Alice;
+    public static BigInteger kAlice;
     
-    public static long bBob;
-    public static long B_Bob;
-    public static long kBob;
+    public static BigInteger bBob;
+    public static BigInteger B_Bob;
+    public static BigInteger kBob;
+    
+    static List<BigInteger> list_prime_num = new ArrayList<>();
+    
+    
     public static void main(String[] args) {
-        p(20);
+        //Известно всем
+        sieveOfEratosthenes();
+        pAlice = BigIntPrimeNumber.generationBigPrimeNum(15);
+        antiderivativeRootModuloP();//gAlice
+        
+        
+        //Закрытые ключи
+        aAlice = BigIntPrimeNumber.nextRandomBigInteger(pAlice);
+        bBob = BigIntPrimeNumber.nextRandomBigInteger(pAlice);
+        System.out.println("aAlice "+aAlice+",  bBob "+bBob);
+        //Открытые личные ключи
+        A_Alice = gAlice.modPow(aAlice, pAlice);
+        B_Bob = gAlice.modPow(bBob, pAlice);
+        System.out.println("A "+A_Alice+",  B "+B_Bob);
+        
+        //Закрытый единый ключь для шифрования
+        kAlice = B_Bob.modPow(aAlice, pAlice);
+        kBob = A_Alice.modPow(bBob, pAlice);
+        System.out.println("K (Alice) "+kAlice+",  K (Bob) "+kBob);
+        
     }
-    private static long p(int bits){
-        long p = generation_prime_num(bits);
-        System.out.println(p);
-        return p;
+    
+    //первообразный корнем по модулю p
+    private static void antiderivativeRootModuloP(){
+        boolean flag = true;
+        HashSet<BigInteger> rems = new HashSet<BigInteger>();
+        for(int i=0;i<list_prime_num.size();i++){
+            System.out.println("-------");
+            flag = true;
+            rems.clear();
+            gAlice = list_prime_num.get(i);
+            for(BigInteger j =BigInteger.ONE ;j.compareTo(pAlice) == -1;j = j.add(BigInteger.ONE)){//i от 1; i<pAlice: i++
+                BigInteger r = gAlice.modPow(j, pAlice);
+                System.out.println("r "+r);
+                if(!rems.contains(r)) rems.add(r);
+                else {
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag)
+                for(BigInteger j =BigInteger.ONE ;j.compareTo(pAlice) == -1;j = j.add(BigInteger.ONE))
+                    if(!rems.contains(j)){flag = false; break;}
+            
+            if(flag) {
+                System.out.println("gAlice "+gAlice);
+                break;
+            }
+        }
+        
+        if(flag == false){
+            gAlice = BigInteger.ZERO;
+            System.out.println("flag "+flag+"  "+"gAlice "+gAlice);
+        }
+    }
+    
+    static public void sieveOfEratosthenes(){
+        int N = 100;
+        boolean[] isPrime  = new boolean[N];
+        Arrays.fill(isPrime,true);
+        isPrime[1] = false;
+        for (int i=2; i*i < N; i++)
+           if (isPrime[i])
+              for (int j=i*i; j < N; j+=i)
+                 isPrime[j] = false;
+        for(int i=1;i<N;i++){
+            if(isPrime[i]){
+                list_prime_num.add( BigInteger.valueOf(i));
+            }
+        }
+        for(BigInteger j : list_prime_num){
+            //System.out.println(j);
+        }
     }
 }
